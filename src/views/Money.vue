418fc7a6700ec075a8e19656e2e37b9a4f9487eb
suweih <475464842@qq.com>
2021-05-21 14:38:36 +0800

    Money的css样式

--- src/views/Money.vue
+++ src/views/Money.vue
@@ -1,5 +1,8 @@
 <template>
-  <div>Money.vue
+  <div class="nav-wrapper">
+    <div class="content">
+      Money.vue
+    </div>
     <Nav/>
   </div>
 </template>
@@ -12,4 +15,17 @@ export default {
 
 <style lang="scss" scoped>
 
+.nav-wrapper {
+  border: 1px solid red;
+  display: flex;
+  flex-direction: column;
+  height: 100vh ;
+  background:#999;
+}
+
+.content {
+  overflow: auto;
+  flex-grow: 1;
+  border: 1px solid blue;
+}
 </style>
