c5e48b82bd842766860fc834ed074b20118a5cd9
suweih <475464842@qq.com>
2021-05-25 23:44:08 +0800

    Id生成器

--- src/views/EditLabel.vue
+++ src/views/EditLabel.vue
@@ -20,11 +20,11 @@ import {Component} from 'vue-property-decorator';
 import tagListModel from '@/model/tagListModel.ts';
 import FromItem from '@/components/Money/FromItem.vue';
 import Button from '@/components/Button.vue';
-
 @Component({
   components: {Button, FromItem}
 })
 export default class EditLabel extends Vue {
+
   tag?: { id: name, name: name } = undefined;
 
   created() {
@@ -46,7 +46,11 @@ export default class EditLabel extends Vue {
   }
   remove(){
     if (this.tag){
-      tagListModel.remove(this.tag.id)
+      if ( tagListModel.remove(this.tag.id)){
+        this.$router.back()
+      }else{
+        window.alert('删除失败')
+      }
     }
   }
   goBack(){
