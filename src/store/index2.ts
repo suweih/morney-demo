41cb137cadd702b33895a9d2234f7580b22abfe3
suweih <475464842@qq.com>
2021-05-26 17:00:35 +0800

    模块化store,删除model文件

--- src/store/index2.ts
+++ src/store/index2.ts
@@ -1,28 +1,8 @@
-import recordListModel from '@/model/recordListModel';
-import tagListModel from '@/model/tagListModel';
+import recordStore from '@/store/recordStore';
+import tagStore from '@/store/tagStore';
 
 const store = {
-  //record Store
-  recordList: recordListModel.fetch(),
-  createRecord: (record: RecordItem) => recordListModel.create(record),
-  //tagList store
-  tagList: tagListModel.fetch(),
-  createTag: (name: string) => {
-    const message = tagListModel.create(name);
-    if (message === 'duplicated') {
-      window.alert('标签名重复');
-    } else if (message === 'success') {
-      window.alert('添加成功');
-    }
-  },
-  removeTag: (id: string) => {
-    return tagListModel.remove(id);
-  },
-  updateTag: (id: string, name: string) => {
-    return tagListModel.update(id, name);
-  },
-  findTag(id: string) {
-    return this.tagList.filter(t => t.id === id)[0];
-  }
+  ...recordStore,
+  ...tagStore
 };
 export default store;
