d0d6583f620175f2ea680ace21dcbd15bd8b5fda
suweih <475464842@qq.com>
2021-05-26 11:27:43 +0800

    将create封装recordListModel

--- src/model/recordListModel.ts
+++ src/model/recordListModel.ts
@@ -1,13 +1,18 @@
+import clone from '@/lib/clone';
+
 const localStorageKeyName = 'recordList';
 
 const recordListModel = {
   data: [] as RecordItem[],
-  clone(data: RecordItem[] | RecordItem) {
-    return JSON.parse(JSON.stringify(data));
+  create(record: RecordItem) {
+    const record2: RecordItem = clone(record);
+    record2.createdAt = new Date();
+    this.data.push(record2);
   },
+
   fetch() {
     this.data = JSON.parse(window.localStorage.getItem(localStorageKeyName) || '[]') as RecordItem[];
-    return this.data
+    return this.data;
   },
   save(data: RecordItem[]) {
     window.localStorage.setItem(localStorageKeyName, JSON.stringify(this.data));
